8b1d16463c354c61cbf86340afb92038f503a96b
Jordan Vinson <jvinson3@cnm.edu>
2016-05-31 10:44:42 -0600

    Connected to server

---
+++ index.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Moparvinson.com</title>
+
+		<meta charset="UTF-8">
+
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+		<!-- fontawesome -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
+
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- CUSTOM CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!-- jQuery -->
+		<script src="https://code.jquery.com/jquery-2.2.3.min.js"   integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo="   crossorigin="anonymous"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+		<style>
+			body {
+				font: 20px Montserrat, sans-serif;
+				line-height: 1.8;
+				color: #f5f6f7;
+			}
+			p {font-size: 16px;}
+			.margin {margin-bottom: 45px;}
+			.bg-1 {
+				background color: #1abc9c; /* THIS IS THE EXAMPLE GREEN COLOR */
+				color: #ffffff;
+			}
+			.bg-2 {
+				background-color: #474e5d; /* DARK BLUE */
+				color: #ffffff;
+			}
+			.bg-3 {
+				background-color: #ffffff; /* WHITE */
+				color: #555555;
+			}
+			.bg-4 {
+				background-color: #2f2f2f; /* GREY */
+				color: #fff;
+			}
+			.container-fluid {
+				padding-top: 70px;
+				padding-bottom: 70px;
+			}
+			.navbar {
+				padding-top: 15px;
+				padding-bottom: 15px;
+				border: 0;
+				border-radius: 0;
+				margin-bottom: 0;
+				font-size: 12px;
+				letter-spacing: 5px;
+			}
+			.navbar-nav li a:hover {
+				color: #1abc9c !important;
+			}
+		</style>
+	</head>
+	<body>
+
+
+		<!-- navbar -->
+		<nav class="navbar navbar-default">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Moparvinson.com</a>
+				</div>
+				<div class="collapse navbar-collapse" id="myNavbar">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#">RESUME</a></li>
+						<li><a href="#">LINKEDIN</a></li>
+						<li><a href="#">GITHUB</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+
+		<!-- container 1 -->
+		<div class="container-fluid bg-1 text-center">
+			<h3 class="margin">Jordan Vinson</h3>
+			<img class="img-circle" src="images/jordan-face.jpg" alt="myheadshot" height="200" widgth="250">
+			<h3>Full Stack Web Developer</h3>
+		</div
+
+
+			<!-- contianer 2 -->
+		<div class="container-fluid bg-2 text-center">
+			<h3 class="margin">Who Am I?</h3>
+			<p>Monterey jack mascarpone boursin. Pepper jack stinking bishop cheese slices cheesy feet cottage cheese cheese and wine melted cheese swiss. Cheese triangles everyone loves when the cheese comes out everybody's happy mascarpone pecorino queso cheesecake the big cheese. Cheese triangles lancashire the big cheese monterey jack.</p>
+			<!-- this is a button, change to the same dropdown as before later -->
+			<a href="#" class="btn btn-default btn-lg">
+				<span class="glyphicon glyphicon-search"></span> Search
+			</a>
+		</div>
+
+
+
+
+
+
+
+
+
+
+
+
+	</body>
+
+	<body class="sfooter">
+		<div class="sfooter-content">
+
+			<header>
+				<div class="container">
+					<!-- begin bootstrap navbar
+					<!-- Brand and toggle get grouped for better mobile display
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu" aria-expanded="false">
+							<span class="sr-only">Other Pages</span>
+							<i class="fa fa-sort-desc fa-2x" aria-hidden="true"></i>
+						</button>
+						<a class="navbar-brand" href="#">Moparvinson.com</a>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling
+					<div class="collapse navbar-collapse" id="main-menu">
+						<ul class="nav navbar-nav navbar-right">
+							<!--change this to a pdf resume
+							<li><a href="http://moparvinson.com/">Resume</a></li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Profiles and Other Items<span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="https://www.linkedin.com/in/jordan-vinson-82387811b?trk=nav_responsive_tab_profile_pic">LinkedIn</a></li>
+									<li><a href="https://github.com/jordanv215"">Github</a></li>
+									<li><a href="Capstone">Capstone</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div><!-- /.navbar-collapse
+				</div>
+			</header>
+
+			<main>
+				<div class="container">
+					<div class="row">
+
+						<!--welcome section
+						<div class="col-xs-12 well text-center">
+							<h1>Jordan Vinson: Full Stack Web Developer</h1>
+							<p>Check out my Resume and other pages from the navbar at the top of the screen. If you'd like to know more, head over to the contact page and ask me anything you'd like.</p>
+						</div>
+
+						<!--1st content section
+						<div class="row">
+							<div class="col-md-3">
+								<div class="well">
+									<h4>BMW</h4>
+									<img class="flt-center img-tiny" src="images/bmw.jpg" alt="bmwbody" height="150" widgth="100">
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="well">
+									<h4>BMW</h4>
+									<img class="flt-center img-tiny" src="images/bmwseats.jpg" alt="bmwseats" height="150" widgth="100">
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="well">
+									<h4>240</h4>
+									<img class="flt-center img-tiny" src="images/240inside.jpg" alt="240inside" height="150" widgth="100">
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="well">
+									<h4>240</h4>
+									<img class="flt-center img-tiny" src="images/240.jpg" alt="240body" height="150" widgth="100">
+								</div>
+							</div>
+						</div>
+
+						<!--2nd content section
+						<div class="row">
+							<div class="col-md-6">
+								<div class="well">
+									<h4>Bavarian Beast</h4>
+									<p>If a demo car is the résumé for a shop – Ultra Box Hiroshima certainly has a good chance of getting called back. Ultra Box of Hiroshima, Japan has been in operation since May of 2002. Ultra Box specializes in European makes, hence the shops two demo cars: this pristine BMW 2002tii and a VW Golf 2. The BMW serves as a rolling representation of the shop’s expertise. Hirotaka Fujiwara set out to build his dream car out of the vintage Bavarian for his own satisfaction along with its commercial purpose.</p>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="well">
+									<h4>JDM</h4>
+									<p>Let’s paint the picture to one of the most exciting and sleepless weeks to ever happen in my life. It’s midnight and I just landed in Medford, Oregon, a small town just a few hours north of the Oregon and California border. The weather is inviting and a complete relief compared to the typical death inducing heat of Phoenix, Arizona. As I make my way curbside I immediately spot the chaperones appointed for my trip; Justin Shreeve and Joe Ayala, better known as their Internet persona Tandem Of Die. Obligatory greetings, handshakes and complaints about long flights are addressed then we’re on our way. Having only heard stories about these guys on the Internet, I really wasn’t sure what to expect for my time here. As I look out the window and gaze upon a dark and quiet Medford, silence is broken by Joe. “Hey, we’re going to make a quick stop at the liquor store ’cause we are running low on beer, it’s going to be a long night working on the cars…”. I knew I was going to get along with these guys!</p>
+								</div>
+							</div>
+						</div>
+					</div><!--/.row
+				</div><!--/.container
+			</main>
+
+		</div>
+
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="foot-content">
+							First bootstrap test
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="foot-content">
+							CSS is fun
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12">
+						<div class="copyright text-center">
+							&copy; 2016 car guru!
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</body>
+
+</html>
