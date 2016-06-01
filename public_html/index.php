7fa0216ddaf3ee632016c0d6fdd591c54a0bba2b
Jordan Vinson <jvinson3@cnm.edu>
2016-06-01 16:36:14 -0600

    Put all styles into css instead of inline

--- public_html/index.php
+++ public_html/index.php
@@ -12,7 +12,8 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
 				integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 		<!--font-->
-		<link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
+		<!--<link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'-->
+		<link href='https://fonts.googleapis.com/css?family=Quattrocento+Sans' rel='stylesheet' type='text/css'>
 
 
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
@@ -39,72 +40,19 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
 				  integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
 				  crossorigin="anonymous"></script>
+<!--I've tried using many fonts, but I've found the default looks the best. I'm going to leave the font tags in the body and in the CSS (as well as the links above in the script tags) just in case I want to change them at some point. Please ignore those for now.-->
 
-		<style>
-			body {
-				font: 20px Montserrat, sans-serif;
-				line-height: 1.8;
-				color: #fdf9ff;
-			}
-
-			p {
-				font-size: 16px;
-			}
-
-			.margin {
-				margin-bottom: 45px;
-			}
-
-			.bg-1 {
-				background color: #1cbcb6;
-				color: #000000;
-			}
-
-			.bg-2 {
-				background-color: #474e5d;
-				color: #fdf9ff;
-			}
-
-			.bg-3 {
-				background-color: #7faebb;
-				color: #555555;
-			}
-
-			.bg-4 {
-				background-color: #789bac;
-				color: #fbffff;
-			}
-
-			.container-fluid {
-				padding-top: 70px;
-				padding-bottom: 70px;
-			}
-
-			.navbar {
-				padding-top: 15px;
-				padding-bottom: 15px;
-				border: 0;
-				border-radius: 0;
-				margin-bottom: 0;
-				font-size: 12px;
-				letter-spacing: 5px;
-			}
-
-			.navbar-nav li a:hover {
-				color: #1abc9c !important;
-			}
-		</style>
 	</head>
 	<body>
 
 
 		<!-- navbar -->
-		<nav class="navbar navbar-default">
+		<nav class="navbar navbar-inverse">
 			<div class="container">
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
 						<span class="icon-bar"></span>
-						<span clas	s="icon-bar"></span>
+						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
 					<a class="navbar-brand" href="#">MOPARVINSON</a>
@@ -201,116 +149,3 @@
 
 		<header>
 			<div class="container">
-				<!-- begin bootstrap navbar
-				<!-- Brand and toggle get grouped for better mobile display
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu" aria-expanded="false">
-						<span class="sr-only">Other Pages</span>
-						<i class="fa fa-sort-desc fa-2x" aria-hidden="true"></i>
-					</button>
-					<a class="navbar-brand" href="#">Moparvinson.com</a>
-				</div>
-
-				<!-- Collect the nav links, forms, and other content for toggling
-				<div class="collapse navbar-collapse" id="main-menu">
-					<ul class="nav navbar-nav navbar-right">
-						<!--change this to a pdf resume
-						<li><a href="http://moparvinson.com/">Resume</a></li>
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Profiles and Other Items<span class="caret"></span></a>
-							<ul class="dropdown-menu">
-								<li><a href="https://www.linkedin.com/in/jordan-vinson-82387811b?trk=nav_responsive_tab_profile_pic">LinkedIn</a></li>
-								<li><a href="https://github.com/jordanv215"">Github</a></li>
-								<li><a href="Capstone">Capstone</a></li>
-							</ul>
-						</li>
-					</ul>
-				</div><!-- /.navbar-collapse
-			</div>
-		</header>
-
-		<main>
-			<div class="container">
-				<div class="row">
-
-					<!--welcome section
-					<div class="col-xs-12 well text-center">
-						<h1>Jordan Vinson: Full Stack Web Developer</h1>
-						<p>Check out my Resume and other pages from the navbar at the top of the screen. If you'd like to know more, head over to the contact page and ask me anything you'd like.</p>
-					</div>
-
-					<!--1st content section
-					<div class="row">
-						<div class="col-md-3">
-							<div class="well">
-								<h4>BMW</h4>
-								<img class="flt-center img-tiny" src="images/bmw.jpg" alt="bmwbody" height="150" widgth="100">
-							</div>
-						</div>
-						<div class="col-md-3">
-							<div class="well">
-								<h4>BMW</h4>
-								<img class="flt-center img-tiny" src="images/bmwseats.jpg" alt="bmwseats" height="150" widgth="100">
-							</div>
-						</div>
-						<div class="col-md-3">
-							<div class="well">
-								<h4>240</h4>
-								<img class="flt-center img-tiny" src="images/240inside.jpg" alt="240inside" height="150" widgth="100">
-							</div>
-						</div>
-						<div class="col-md-3">
-							<div class="well">
-								<h4>240</h4>
-								<img class="flt-center img-tiny" src="images/240.jpg" alt="240body" height="150" widgth="100">
-							</div>
-						</div>
-					</div>
-
-					<!--2nd content section
-					<div class="row">
-						<div class="col-md-6">
-							<div class="well">
-								<h4>Bavarian Beast</h4>
-								<p>If a demo car is the résumé for a shop – Ultra Box Hiroshima certainly has a good chance of getting called back. Ultra Box of Hiroshima, Japan has been in operation since May of 2002. Ultra Box specializes in European makes, hence the shops two demo cars: this pristine BMW 2002tii and a VW Golf 2. The BMW serves as a rolling representation of the shop’s expertise. Hirotaka Fujiwara set out to build his dream car out of the vintage Bavarian for his own satisfaction along with its commercial purpose.</p>
-							</div>
-						</div>
-						<div class="col-md-6">
-							<div class="well">
-								<h4>JDM</h4>
-								<p>Let’s paint the picture to one of the most exciting and sleepless weeks to ever happen in my life. It’s midnight and I just landed in Medford, Oregon, a small town just a few hours north of the Oregon and California border. The weather is inviting and a complete relief compared to the typical death inducing heat of Phoenix, Arizona. As I make my way curbside I immediately spot the chaperones appointed for my trip; Justin Shreeve and Joe Ayala, better known as their Internet persona Tandem Of Die. Obligatory greetings, handshakes and complaints about long flights are addressed then we’re on our way. Having only heard stories about these guys on the Internet, I really wasn’t sure what to expect for my time here. As I look out the window and gaze upon a dark and quiet Medford, silence is broken by Joe. “Hey, we’re going to make a quick stop at the liquor store ’cause we are running low on beer, it’s going to be a long night working on the cars…”. I knew I was going to get along with these guys!</p>
-							</div>
-						</div>
-					</div>
-				</div><!--/.row
-			</div><!--/.container
-		</main>
-
-	</div>
-
-	<footer>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-6">
-					<div class="foot-content">
-						First bootstrap test
-					</div>
-				</div>
-				<div class="col-md-6">
-					<div class="foot-content">
-						CSS is fun
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-xs-12">
-					<div class="copyright text-center">
-						&copy; 2016 car guru!
-					</div>
-				</div>
-			</div>
-		</div>
-	</footer>
-</body>
-
-</html>
