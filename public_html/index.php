d628b3b4261f1025f0d6cfc44897743262fd2ae4
Jordan Vinson <jvinson3@cnm.edu>
2016-06-09 08:22:32 -0600

    Finalized PWP personal website

--- public_html/index.php
+++ public_html/index.php
@@ -41,8 +41,16 @@
 				  integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
 				  crossorigin="anonymous"></script>
 <!--I've tried using many fonts, but I've found the default looks the best. I'm going to leave the font tags in the body and in the CSS (as well as the links above in the script tags) just in case I want to change them at some point. Please ignore those for now.-->
-		<script src='https://www.google.com/recaptcha/api.js'></script>
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
 
+		<!-- Your JavaScript Form Validator -->
+		<script src="js/form-validate.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
 	</head>
 	<body>
 
@@ -75,14 +83,13 @@
 			<img class="img-circle" src="image/jordan-face.jpg" alt="myheadshot" height="200" width="200">
 			<h3>Full Stack Web Developer</h3>
 		</div
-
-
+			
 			<!-- contianer 2 -->
 		<div class="bg-2 text-center">
 			<div class="container">
 				
 			<h3 class="margin">Who Am I?</h3>
-			<p>I take pride in calling myself a full stack web developer. I am starting my web development career with a proficient background in both front and back end coding. I am very excited to learn and become even more experienced in a career category that lets me create exactly what I see in my mind. If you want to know more about my web development experience, please click the dropdowns below.
+			<p>I am a full stack web developer. I am advancing my web development career with a proficient background in both front and back end coding. I am very excited to learn and become even more experienced in a career category that lets me create exactly what I see in my mind. If you want to know more about my web development experience, please click the dropdowns below.
 			</p>
 				<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
 					<div class="panel panel-default">
@@ -109,9 +116,9 @@
 						</div>
 						<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
 							<div class="panel-body">
-								As a starting full stack web developer, my goal is to commit myself to learning more through experience and
-								work. I'm very interested in developing anything from applications and games, to websites and programs. My favorite
-								development is more centered towards the front end of websites. I'm hoping to find employment at a startup or large company, although I am also prepared for free-lance work as long as it gives me more experience. If you'd like to contact me with any
+								As a full stack web developer, my goal is to commit myself to learning more through experience and
+								work. I'm very interested in developing anything from applications and games, to websites and software. My favorite
+								development is more centered towards the front end of websites. I'm hoping to find employment at a startup or large company, although I am also prepared for freelance work as long as it gives me the opportunity to show my enthusiasm to learn, gain experience, and lets me display my talents. If you'd like to contact me with any
 								questions, my info will be at the footer of this site. Alternatively, you can check my LinkedIn or my Resume
 								at the top of the page. You can also check my Github profile to see other projects I've worked on, which is
 								also linked at the top of the page. Thanks for viewing my site!
@@ -121,7 +128,6 @@
 			</div>
 		</div>
 			
-
 		<!-- container 3 -->
 		<div class="bg-3 text-center">
 			<div class="container">
@@ -129,18 +135,18 @@
 				<div class="row">
 					<div class="col-sm-4">
 						<p>I attended Deep Dive Coding Bootcamp at the CNM STEMulus Center in order to gain experience in web development and give me a diverse background of skills. Throughout the class, I was pushed to create many different things, including this web page. The main goal of the class was to prepare the alumni for a career in coding, and have them graduate the class with the skills and experience necessary for a starting position that can set their career off strong.</p>
-						<a href="https://bootcamp-coders.cnm.edu/">
+						<a href="http://stemuluscenter.org/deep-dive-fullstack/">
 						<img class="img-circle" src="image/deep-dive.png" alt="deepdive" height="100" width="100">
 						</a>
 					</div>
 					<div class="col-sm-4">
-						<p>During the Deep Dive Bootcamp, myself and 3 other students created a Capstone project called Red Rovr. The basis was to take NASA's data and images from the Curiosity Rover and remodel them in an easy to read format. Basically, we used the NASA Api, and made a dashboard for images and news, along with other functionality such as weather and social media aspects. If you'd like to check it out, head over to redrovr.io and view our work. Maybe you'll gain an interest for Mars.</p>
+						<p>During the Deep Dive Bootcamp, my three teammates and I created a Capstone project called Red Rovr. The basis was to take NASA's data and images from the Curiosity Rover and remodel them in an easy to read format. Basically, we used the NASA API, and made a dashboard for images and news, along with other functionality such as weather and social media aspects. If you'd like to check it out, head over to redrovr.io and view our work. Maybe you'll gain an interest for Mars.</p>
 						<a href="https://docs.google.com/drawings/d/1n_S-RLN5OAZTVJ1TNv-z-l-6mU6IkQefEvtCd4ncwYU/edit">
 						<img class="img-circle" src="image/hubble-mars-opposition.jpg" alt="mars" height="100" width="100">
 						</a>
 					</div>
 					<div class="col-sm-4">
-						<p>I started off my life as a mechanic. Since I was 4, my father and I would watch car movies and then go out and work on his many cars. When I was just 16, I completely rebuilt and restored my very first car, a 1974 Dodge Charger SE, along with some help from my dad. Cars and the community based around them will always be something I love, but I knew it wasn't a smart career path. They're still my passion, I just moved from building cars to building websites (click the wrench).</p>
+						<p>I started off my life as a mechanic. Since I was four, my father and I would watch car movies and then go out and work on his many cars. When I was just 16, I completely rebuilt and restored my very first car, a 1974 Dodge Charger SE, along with some help from my dad. Cars and the community based around them will always be something I love, but I knew it wasn't a smart career path. They're still my passion, I just moved from building cars to building websites (click the wrench).</p>
 						<img class="img-circle" src="image/wrench-idk.png" alt="wrench" height="100" width="100" data-toggle="modal" data-target="#myModal">
 						<div class="container">
 
@@ -154,30 +160,19 @@
 											<img src="image/car-and-me-for-website.jpg" alt="my 74 charger" width="1024px" height="682px">
 										</div>
 									</div>
-
 								</div>
 							</div>
-
 						</div>
-
-
+						
 							</div>
 						</div>
-						
-						
-						
-						
-						
-						
 					</div>
 
 			<div class="modal fade" id="contact-form-modal" tabindex="-1" role="dialog">
 				<div class="modal-dialog">
 					<div class="modal-content">
 						<div class="modal-body">
-
-
-
+							
 							<!--Begin Contact Form-->
 							<form id="contact-form" action="php/mailer.php" method="post">
 								<div class="form-group">
@@ -234,13 +229,6 @@
 					</div><!-- /.modal-content -->
 				</div><!-- /.modal-dialog -->
 			</div><!-- /.modal -->
-
-					
-					
-					
-					
-					
-					
 				</div>
 			</div>
 		</div>
